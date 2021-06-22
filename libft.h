e8b581a1f6c2db18b17bcee833424642d9489458
jnaftana <jnaftana@student.42madrid.com>
2021-06-22 19:35:16 +0200

    mod header

--- libft.h
+++ libft.h
@@ -30,8 +30,8 @@ void 			*ft_calloc(size_t nitems, size_t size);
 char			*ft_strdup(const char *str);
 char			*ft_substr(char const *str, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *str, char c);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *str, char c);
 
 void			ft_putstr(char *str);
 #endif
