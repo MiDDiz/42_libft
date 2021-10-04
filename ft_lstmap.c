24771126bac4863d8b9613fee886ea7a45c9dc7b
MiDDiz <xxmidone30xx@gmail.com>
2021-10-04 12:35:10 +0200

    ready for deploy

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jnaftana <jnaftana@student.42madrid.es>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 11:23:11 by jnaftana          #+#    #+#             */
-/*   Updated: 2021/10/02 10:56:33 by jnaftana         ###   ########.fr       */
+/*   Updated: 2021/10/04 12:30:59 by jnaftana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
-	t_list	*h_ptr;
+	t_list	*ptr_helper;
 
 	head = ft_lstnew((*f)(lst->content));
-	h_ptr = head;
 	lst = lst->next;
 	while (lst)
 	{
-		h_ptr->next = ft_lstnew((*f)(lst->content));
-		if (!h_ptr->next)
+		ptr_helper = ft_lstnew((*f)(lst->content));
+		if (!ptr_helper)
 		{
 			ft_lstclear(&head, (*del));
 			return (head);
 		}
-		lst = lst->next;
+	lst = lst->next;
+	ft_lstadd_back(&head, ptr_helper);
 	}
 	return (head);
 }
